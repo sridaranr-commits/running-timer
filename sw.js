34f2e99ff4d2ed6c3cb05fdfcb805ea8696c829d
sridaranr-commits <sridaranr@gmail.com>
2026-09-19 23:57:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="running-timer-v10";
+const CACHE="running-timer-v15";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
